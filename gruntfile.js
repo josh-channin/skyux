ee9828873f50642be89996c2e58ff6a16fbd3189
Josh Channin <joshua.channin@gmail.com>
2016-07-17 13:36:00 -0400

    update files

--- gruntfile.js
+++ gruntfile.js
@@ -38,5 +38,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-ngdocs');
     grunt.loadNpmTasks('grunt-sri');
+    grunt.loadNpmTasks('grunt-scss-lint');
     grunt.loadNpmTasks('grunt-webdriver');
 };
